91b458db87169b35966295fe907589306fb9efc7
JUNGUIHEON <jungui4366@gmail.com>
2024-08-13 11:48:02 +0900

    feat:[정의헌] authentication domain 추가 [TFF-7]

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import boardModule from "@/board/store/boardModule";
 import accountModule from '@/account/store/accountModule';
+import authenticationModule from '@/authentication/store/authenticationModule';
 
 export default createStore({
   state: {
@@ -13,6 +14,7 @@ export default createStore({
   },
   modules: {
     boardModule,
-    accountModule
+    accountModule,
+    authenticationModule,
   }
 })
