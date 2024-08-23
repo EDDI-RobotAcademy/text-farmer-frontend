17c7bf1076e540a2411d6d371e86072dfdcca3a8
0H00NI <yhoon3002@gmail.com>
2024-08-23 14:25:04 +0900

    style: [임영훈] 전역 폰트 설정 [TFF-x]

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <v-app>
-    <navigation-menu-bar/>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <navigation-menu-bar />
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import NavigationMenuBar from './navigationBar/NavigationMenuBar.vue'
+import { defineComponent } from "vue";
+import NavigationMenuBar from "./navigationBar/NavigationMenuBar.vue";
 
 export default defineComponent({
-  components: { NavigationMenuBar },
-  name: 'App',
+    components: { NavigationMenuBar },
+    name: "App",
 
-  data () {
-    return {
-      //
-    }
-  },
-})
+    data() {
+        return {
+            //
+        };
+    },
+});
 </script>
+
+<style>
+* {
+    font-family: "OAGothic", sans-serif !important;
+}
+
+@font-face {
+    font-family: "OAGothic";
+
+    src: url("assets/fonts/OAGothic-Medium.ttf") format("truetype");
+
+    font-weight: 500;
+}
+
+@font-face {
+    font-family: "OAGothic";
+
+    src: url("assets/fonts/OAGothic-ExtraBold.ttf") format("truetype");
+
+    font-weight: 800;
+}
+</style>
